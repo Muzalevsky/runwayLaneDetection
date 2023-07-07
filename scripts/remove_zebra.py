3589d8e5145b0b17134e2d3a1268da2fc566ff8d
Elena Serykh <elena.serykh@waveaccess.ru>
2023-07-07 16:24:19 +0300

    [RF]: refactoring of script remove_zebra

--- scripts/remove_zebra.py
+++ scripts/remove_zebra.py
@@ -10,19 +10,24 @@ from lane_detection_medium.utils.fs import get_date_string, read_txt, save_txt
 from lane_detection_medium.utils.load import get_zebra_id
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",  # noqa: WPS323
 )
 logger = logging.getLogger(__file__)
 
 
 @click.command()
 @click.option(
-    "-v", "--dataset-version", required=True, type=str, help="The name of the dataset folder"
+    "-v",
+    "--dataset-version",
+    required=True,
+    type=str,
+    help="The name of the dataset folder",
 )
 def main(dataset_version: str):
     src_dpath = paths.yolo_dpath / "data" / dataset_version
     if not src_dpath.exists():
-        raise ValueError(f"Dataset '{dataset_version}' does not exist.")
+        raise ValueError(f"Dataset {dataset_version!r} does not exist.")
 
     out_dpath = paths.yolo_dpath / "data" / get_date_string()
     out_dpath.mkdir(parents=True, exist_ok=True)
@@ -37,15 +42,15 @@ def main(dataset_version: str):
         out_mode_labels_dpath = out_mode_dpath / "labels"
         out_mode_labels_dpath.mkdir(parents=True, exist_ok=True)
 
-        zebra_id = str(get_zebra_id())
-
-        mode_txt_fpaths = sorted(list((src_mode_dpath / "labels").glob("*.txt")))
-        stream = tqdm(mode_txt_fpaths, desc=f"{mode.name.capitalize()} txt processing")
+        stream = tqdm(
+            sorted((src_mode_dpath / "labels").glob("*.txt")),
+            desc=f"{mode.name.capitalize()} txt processing",  # noqa: WPS237
+        )
         for txt_fpath in stream:
             src_labels = read_txt(txt_fpath)
 
             # NOTE: "zebra" boxes are excluded
-            out_labels = [label for label in src_labels if label[0] != zebra_id]
+            out_labels = [label for label in src_labels if label[0] != get_zebra_id()]
             if not len(out_labels):
                 # NOTE: do not save empty txt files
                 continue
